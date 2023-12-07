ee57f40bb3d2c6750538c3afd2e75b4f02aa9b04
otTATto <TATo123SU@gmail.com>
2023-12-07 14:41:44 +0900

    Contents transition in tests page

---
+++ js/teacher/tests.js
@@ -0,0 +1,30 @@
+// ナビにおける「本日のテスト」ボタンを押したときに実行
+function viewTodayTestArea(){
+    // 「本日のテスト」を表示・「今後のテスト」を非表示・「過去のテスト」を非表示
+    $('#todayTestArea').removeClass('unvisible').addClass('visible');
+    $('#futureTestArea').removeClass('visible').addClass('unvisible');
+    $('#pastTestArea').removeClass('visible').addClass('unvisible');
+}
+
+// ナビにおける「今後のテスト」ボタンを押したときに実行
+function viewFutureTestArea(){
+    // 「本日のテスト」を非表示・「今後のテスト」を表示・「過去のテスト」を非表示
+    $('#todayTestArea').removeClass('visible').addClass('unvisible');
+    $('#futureTestArea').removeClass('unvisible').addClass('visible');
+    $('#pastTestArea').removeClass('visible').addClass('unvisible');
+}
+
+// ナビにおける「過去のテスト」ボタンを押したときに実行
+function viewPastTestArea(){
+    // 「本日のテスト」を非表示・「今後のテスト」を非表示・「過去のテスト」を表示
+    $('#todayTestArea').removeClass('visible').addClass('unvisible');
+    $('#futureTestArea').removeClass('visible').addClass('unvisible');
+    $('#pastTestArea').removeClass('unvisible').addClass('visible');
+}
+
+
+// 関数のエクスポート
+window.viewTodayTestArea = viewTodayTestArea;
+window.viewFutureTestArea = viewFutureTestArea;
+window.viewPastTestArea = viewPastTestArea;
+export{ viewTodayTestArea, viewFutureTestArea, viewPastTestArea }
