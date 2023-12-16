edd66738dee172baed06837aa75cab2ec5a7ad2c
maibo <sorakado222@gmail.com>
2023-12-16 16:36:25 +0900

    教師側testsにおいて科目の検索をしたら その科目のテストが日付に応じて振り分けられて表示されるように実装

--- js/teacher/tests.js
+++ js/teacher/tests.js
@@ -27,6 +27,10 @@ const database = getDatabase();
 import { queryDivider, generateUuid } from '../set.js';
 
 var uidValue;   //自分(教師)のuidを格納
+var subjectsList = document.getElementById('subjChoose');   //科目選択のプルダウン、表示エリア(親クラス)
+var todayTestsArea = document.getElementById('todayTestArea');  //今日のテストの表示エリア
+var futureTestsArea = document.getElementById('futureTestArea');    //今後のテストの表示エリア
+var pastTestsArea = document.getElementById('pastTestArea');    //過去のテストの表示エリア
 
 // 起動時に実行
 window.addEventListener('load', function(){
@@ -39,8 +43,7 @@ window.addEventListener('load', function(){
 // 起動時に自分の担当している教科を「科目選択」のプルダウンに追加→テスト追加の科目名にも適用, プルダウンのvalueは教科のuid
 window.addEventListener('load', async function(){
     // DB.subjectsからmanagerIdが自分になっている教科を探し出す(全探索)
-    var subjectsList1 = document.getElementById('subjChoose');   //科目選択の表示エリア(親クラス)
-    var subjectsList2 = document.getElementById('subjTest');    //テスト追加の科目選択の表示エリア(親クラス2)
+    var subjectsListForAdd= document.getElementById('subjTest');    //テスト追加の科目選択のプルダウン、表示エリア(親クラス2)
     const subjRef = ref(database, 'subjects/');
     var subjSnapshot = await get(subjRef);
     var subjData = subjSnapshot.val();
@@ -58,8 +61,8 @@ window.addEventListener('load', async function(){
             var subject2 = document.createElement('option');
             subject2.value = subjUid;
             subject2.text = subjName;
-            subjectsList1.appendChild(subject);   //エリアに追加
-            subjectsList2.appendChild(subject2);
+            subjectsList.appendChild(subject);   //エリアに追加
+            subjectsListForAdd.appendChild(subject2);
         }
     });
 
@@ -81,7 +84,178 @@ window.addEventListener('load', function(){
 
 })
 
-// 科目検索のプルダウンを選択したときにテストを絞り込む
+
+// 科目検索のプルダウンを選択したときにその科目のテストを表示する
+subjectsList.addEventListener('change', async function(){
+    // 選択された科目のuidを取得
+    var selectedSubjUid = subjectsList.value;
+    // テストエリアをクリアにする
+    todayTestsArea.innerHTML = `<div class="mt-3">
+                                    <div class="row justify-content-center row-cols-auto f-Zen-Kaku-Gothic-New">
+                                        <div class="col">
+                                            <div class="fw-exbold c-pink tb-pink">
+                                                本日のテスト
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <div onclick="viewFutureTestArea()" type="button" class="fw-medium text-secondary be-big-lg">
+                                                今後のテスト
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <div onclick="viewPastTestArea()" type="button" class="fw-medium text-secondary be-big-lg">
+                                                過去のテスト
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="mt-4"></div>`;
+    futureTestsArea.innerHTML = `<div class="mt-3">
+                                    <div class="row justify-content-center row-cols-auto f-Zen-Kaku-Gothic-New">
+                                        <div class="col">
+                                            <div onclick="viewTodayTestArea()" type="button" class="fw-medium text-secondary be-big-lg">
+                                                本日のテスト
+                                            </div>
+                                            
+                                        </div>
+                                        <div class="col">
+                                            <div class="fw-exbold c-pink tb-pink">
+                                                今後のテスト
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <div onclick="viewPastTestArea()" type="button" class="fw-medium text-secondary be-big-lg">
+                                                過去のテスト
+                                            </div>
+                                        </div>
+                                    </div> 
+                                </div>
+
+                                <div class="mt-4"></div>`;
+    pastTestsArea.innerHTML = `<div class="mt-3">
+                                    <div class="row justify-content-center row-cols-auto f-Zen-Kaku-Gothic-New">
+                                        <div class="col">
+                                            <div onclick="viewTodayTestArea()" type="button" class="fw-medium text-secondary be-big-lg">
+                                                本日のテスト
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <div onclick="viewFutureTestArea()" type="button" class="fw-medium text-secondary be-big-lg">
+                                                今後のテスト
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <div class="fw-exbold c-pink tb-pink">
+                                                過去のテスト
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="mt-4"></div>`;
+    
+
+    // 選択された科目の各テストの情報を取得(fetchTestData)
+    var tests = await fetchTestsData(selectedSubjUid);
+    // 取得したデータをもとにボタンを追加
+    tests.forEach(test => {
+        var newTestElement = document.createElement("div");
+        newTestElement.setAttribute("type", "button");
+        newTestElement.setAttribute("data-bs-toggle", "modal");
+        newTestElement.setAttribute("data-bs-target", "#testViewModal");
+        newTestElement.classList.add("shadow", "br-10", "mt-2", "mx-lg-5", "mx-3", "px-3", "py-3", "f-Zen-Kaku-Gothic-New", "be-big-sm");
+        newTestElement.style.border = "2px solid rgb(124, 154, 95)";
+
+        newTestElement.innerHTML = `
+            <div class="row row-cols-auto">
+                <div class="col fw-bold br-20 px-3 ms-2 text-white" style="background-color: rgb(124, 154, 95); font-size: 20px;">
+                    ${test.subject}
+                </div>
+                <div class="col fw-exbold c-black" style="font-size: 20px">
+                    ${test.title}
+                </div>
+            </div>
+            <div class="my-2" style="border-bottom: 1px solid rgb(180, 180, 180);"></div>
+            <div class="row row-cols-auto mt-1 justify-content-end">
+                <div class="col f-Zen-Maru-Gothic br-20 px-3 ms-2 text-white" style="background-color: rgb(89, 121, 60); font-size: 15px;">
+                    ${test.date}
+                </div>
+                <div class="col f-Zen-Maru-Gothic fw-medium c-black text-secondary" style="font-size: 15px">
+                    ${test.examinees}
+                </div>
+            </div>
+        `;
+
+    // テストの日時に応じてエリアを振り分ける
+    var testUnixTime = new Date(test.date).getTime()/1000//テスト開始時間のunixTime
+    var nowUnixTime = Date.now();     //現在時間(unix time)
+    var now = new Date(nowUnixTime);    //Dateオブジェクトに変換
+    var nowYear = now.getFullYear();    //現在年
+    var nowMonth = now.getMonth() + 1;      //現在月
+    var nowDay = now.getDate();     //現在日
+    var todayUnixTime = new Date(nowYear, nowMonth -1, nowDay).getTime()/1000; //今日00:00のunixTime
+    var tomorrowUnixTime = todayUnixTime + 86400;    //明日の00:00のUnixTime(1日のunixTime=86400)
+    // console.log('現在' + nowYear + '年' + nowMonth + '月' + nowDay + '日');
+    // console.log('テスト' + testYear + '年' + testMonth + '月' + testDay + '日');
+
+    if(testUnixTime >= todayUnixTime && testUnixTime < tomorrowUnixTime){   //テストが今日だったら
+        todayTestsArea.appendChild(newTestElement);
+    } else if(testUnixTime >= tomorrowUnixTime){   //テストが明日以降だったら
+        futureTestsArea.appendChild(newTestElement);
+    } else{ //テストが昨日以前だったら
+        pastTestsArea.appendChild(newTestElement);
+    }
+
+    });
+
+})
+
+async function fetchTestsData(selectedSubject) {
+    console.log("selectedSubject :" + selectedSubject)
+    const subjRef = ref(database, 'subjects/' + selectedSubject + '/tests');
+    const subjSnapshot = await get(subjRef);
+    const subjData = subjSnapshot.val();
+    console.log("subjData :" + subjData)
+    const tests = [];
+
+    const subjRef1 = ref(database, 'subjects/' + selectedSubject+'/');
+    const subjSnapshot1 = await get(subjRef1);
+    const subjData1 = subjSnapshot1.val();
+    console.log("subjData1 :" + subjData1)
+    
+    // var subbject = subjData1.mainData.subjectName;
+    // var sumstu  = numberOfParticipants;
+
+    // パスを指定してデータを取得
+    const subjRef3 = ref(database, 'subjects/' + selectedSubject + '/participants');
+    const subjSnapshot3 = await get(subjRef3);
+    const participantsData3 = subjSnapshot3.val(); 
+    // オブジェクトのキー（参加者のID）の数を取得
+    const numberOfParticipants = Object.keys(participantsData3).length;
+
+    var subbject = subjData1.mainData.subjectName;
+    var sumstu  = numberOfParticipants;
+    if (subjData) { //テストが存在したら
+        // Firebaseから取得したデータをtests配列に変換
+        Object.keys(subjData).forEach(element => {
+            const testInfo = {
+                subject: subbject,
+                title: subjData[element].mainData.testName,
+                date: subjData[element].mainData.testDate,
+                examinees: '受験予定者：'+ sumstu + '人'
+            };
+
+            tests.push(testInfo);
+        });
+    } else {
+        console.log("subjData is null or undefined");
+    }
+
+    return tests;
+}
+
+
 
 // 新しくテストを作成する
 async function makeTest(){
